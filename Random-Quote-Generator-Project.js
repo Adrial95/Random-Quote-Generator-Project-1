c46b2b797b97edbda944f98a57a913b37d2c9f6b
Adrial95 <avaheer@gmail.com>
2020-05-17 17:37:22 -0700

    updated project sunday 17th may 2020

--- Random-Quote-Generator-Project.js
+++ Random-Quote-Generator-Project.js
@@ -8,9 +8,9 @@ var red;
 var green;
 var blue;
 var colors;
-var randomQuote = 0;
+var randomQuote;
 var getQuote;
-var randomNumber;
+var randomNumber = 0;
 var message = '';
 
 /*** 
@@ -18,27 +18,27 @@ var message = '';
 ***/
 var quotes = [
 {quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-source: ' - Nelson Mandela',
+source: '  Nelson Mandela',
 citation: 'https://blog.hubspot.com/sales/famous-quotes',
 },
 
 {quote:'The way to get started is to quit talking and begin doing.',
-source: '- Walt Disney',
+source: ' Walt Disney',
 citation: 'https://blog.hubspot.com/sales/famous-quotes',
 },
 
  {quote: 'Your time is limited, so dont waste it living someone elses life. Dont be trapped by dogma.',
-  source: '- steve Jobs',
+  source: ' steve Jobs',
   citation: 'https://blog.hubspot.com/sales/famous-quotes',
 },
 
 {quote: 'If life were predictable it would cease to be life, and be without flavor.',
-  source: '- Eleanor Roosevelt',
+  source: ' Eleanor Roosevelt',
   citation: 'https://blog.hubspot.com/sales/famous-quotes',
 },
 
 { quote: 'If you look at what you have in life, youll always have more. If you look at what you dont have in life, youll never have enough.',
-  source: '- Oprah Winfrey',
+  source: ' Oprah Winfrey',
   citation: 'https://blog.hubspot.com/sales/famous-quotes',
 }
 
@@ -48,14 +48,10 @@ citation: 'https://blog.hubspot.com/sales/famous-quotes',
 /***
  * `getRandomQuote` function
 ***/
-function getRandomQuote(arr) {
-var randomNumber = math.floor(math.random() * (quotes.length));
-listHTML = '</ol>';
-for(var i = 0; i < arr.length;i += 1) {
-istHTML += '<li>'  + arr[i] + '</li>';
-}
-listHTML = '</ol>';
-return listHTML;
+function getRandomQuote() {
+var randomNumber = Math.floor(Math.random() * (quotes.length));
+
+return quotes[randomNumber];
 }
 
 //the function bellow generates a random color everytime a new quote loads//
@@ -65,37 +61,32 @@ var red = math.floor(math.random() * 256);
 var green = math.floor(math.random() * 256);
 var blue = math.floor(math.random() * 256);
 var colors = 'rgb(' + red +','+ green +', '+ blue +')';
-return colors;
+return getRandomColor();
 };
 
 /***
  * `printQuote` function
 ***/
-function printQuote(message) {
-var outputDiv = document.getElementById('output');
-outputDiv.innerHTML = message;
+function printQuote() {
 
+  const quote = getRandomQuote();
+let html='';
 
-html=`<p class="quote">${quotes.quote}</p>`;
-    html+=`<p class="source">${quotes.source}</p>`;
-    html+= `<p class="citation">${quotes.citation}<p/>`;
-};
-while (true) {
-    for (var i = 0; i < quotes.length; i += 1) {
-        getQuote = quotes[i];
-        console.log(message);
-       var randomQuote =+ [i];
-        
-}
-if (quotes.citation) {
-  html+=`<span class="citation">${quotes.citation}</span>`;
+if (quote.citation) {
+  html+=`<span class="citation">${quote.citation}</span>`;
 }
 if (quotes.source) {
-  html+=`<span class="source">${quotes.source}</span>`;
-  print(message);
-  newFunction();
+  html+=`<span class="source">${quote.source}</span>`;
 }
+
+html=`<p class="quote">${quote.quote}</p>`;
+    html+=`<p class="source">${quote.source}</p>`;
+    html+= `<p class="citation">${quote.citation}<p/>`;
+
+    html+='</p>';
+    document.getElementById('quote-box').innerHTML = html;
 };
+
 
 
 /***
